f852a469c09590c70cd2436289a0e3dc2da97133
rwiteshbera <rwiteshbera@gmail.com>
2021-09-04 21:55:10 +0530

    Added README.md

--- portfolio/src/components/Contacts.js
+++ portfolio/src/components/Contacts.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/Contact.css";
 
 class Contacts extends Component {
@@ -18,50 +18,106 @@ class Contacts extends Component {
 
   handleChange = (event) => {
     const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.type === "checkbox" ? target.checked : target.value;
     const name = target.name;
     this.setState({
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
 
   handleSubmit = (event) => {
     event.preventDefault();
 
     this.setState({
       disabled: true,
-      emailSent: true
-    })
-  }
+      emailSent: true,
+    });
+  };
   render() {
     return (
-      <div className="contact-div" id='contact'>
+      <div className="contact-div" id="contact">
         <h1>Let's Talk</h1>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group>
-              <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                <Form.Control id="full-name" name="name" type='text' value={this.state.name}
-                onChange={this.handleChange}></Form.Control>
+            <Form.Label htmlFor="full-name">Full Name</Form.Label>
+            <Form.Control
+              id="full-name"
+              name="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            ></Form.Control>
           </Form.Group>
 
           <Form.Group>
-              <Form.Label htmlFor="email">Email</Form.Label>
-                <Form.Control id="email" name="email" type='email' value={this.state.email}
-                onChange={this.handleChange}></Form.Control>
+            <Form.Label htmlFor="email">Email</Form.Label>
+            <Form.Control
+              id="email"
+              name="email"
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            ></Form.Control>
           </Form.Group>
 
           <Form.Group>
-              <Form.Label htmlFor="message">Message</Form.Label>
-                <Form.Control id="message" name="message" as='textarea' rows='5' value={this.state.message}
-                onChange={this.handleChange}></Form.Control>
+            <Form.Label htmlFor="message">Message</Form.Label>
+            <Form.Control
+              id="message"
+              name="message"
+              as="textarea"
+              rows="5"
+              value={this.state.message}
+              onChange={this.handleChange}
+            ></Form.Control>
           </Form.Group>
 
-          <Button id="submit" className="d-inline-block" type='submit' disabled={this.state.disabled}>Submit</Button>
+          <Button
+            id="submit"
+            className="d-inline-block"
+            type="submit"
+            disabled={this.state.disabled}
+          >
+            Submit
+          </Button>
         </Form>
 
         {/* Feedback after submission */}
-        {this.state.emailSent === true && <p className="d-inline success-msg"> Email Sent </p>}
-        {this.state.emailSent === false && <p className="d-inline err-msg"> Email Not Sent </p>}
+        {this.state.emailSent === true && (
+          <p className="d-inline success-msg"> Email Sent </p>
+        )}
+        {this.state.emailSent === false && (
+          <p className="d-inline err-msg"> Email Not Sent </p>
+        )}
+
+        <div className="social">
+          <a
+            href="https://github.com/rwiteshbera"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button variant="warning">Github</Button>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/rwitesh-bera/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Button variant="warning" id="btn">
+              Linkedin
+            </Button>
+          </a>
+          <a
+            href="https://twitter.com/RwiteshBera"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <Button variant="warning" id="btn">
+              Twitter
+            </Button>
+          </a>
+        </div>
       </div>
     );
   }
